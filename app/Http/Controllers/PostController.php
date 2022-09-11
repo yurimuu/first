b8a048a21c479447dd92b84ec7c9be6fa1c945de
yurimuu <yunoopy1318@gmail.com>
2022-09-12 01:36:21 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,12 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return $post->get();
+    return view('posts/index')->with(['posts' => $post->get()]);
+    }
+    public function show(Post $post)
+    {
+    return view('posts/show')->with(['post' => $post]);
     }
 }
 
+?>
