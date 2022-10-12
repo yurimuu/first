fd5d8382faba90d0f78eba539bbe3eab49b0c08e
yurimuu <yunoopy1318@gmail.com>
2022-10-12 20:51:01 +0900

    [add]認証機能

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,29 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    
-    <body>
-        <h1>Blog Name</h1>
-        <p class='create'>[<a href='/posts/create'>create</a>]</p>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                        <a href="">{{ $post->category->name }}</a>
-                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+
+    {{Auth::user()->name}}
+    <h1>Blog Name</h1>
+    <p class='create'>[<a href='/posts/create'>create</a>]</p>
+    <div class='posts'>
+        @foreach ($posts as $post)
+            <div class='post'>
+                <h2 class='title'>
+                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                </h2>
+                    <a href="">{{ $post->category->name }}</a>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                <p class='body'>{{ $post->body }}</p>
+            </div>
+        @endforeach
+    </div>
+    <div class='paginate'>
+        {{ $posts->links() }}
+    </div>
+@endsection
